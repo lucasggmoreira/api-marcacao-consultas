ce4ada099f1aa6421eac683e366876d17be0ec7d
Lucas Moreiram <lggm18@hotmail.com>
2025-03-11 19:06:51 -0300

    style: renomeado dto dos detalhes do usuário

--- src/main/java/com/fiap/ecr/api_marcacao_consultas/dto/DadosUsuarioDetalhado.java
+++ src/main/java/com/fiap/ecr/api_marcacao_consultas/dto/DadosUsuarioDetalhado.java
@@ -3,14 +3,14 @@ package com.fiap.ecr.api_marcacao_consultas.dto;
 import com.fiap.ecr.api_marcacao_consultas.model.TipoUsuario;
 import com.fiap.ecr.api_marcacao_consultas.model.Usuario;
 
-public record DadosUsuarioCriado(
+public record DadosUsuarioDetalhado(
         Long id,
         String nome,
         String email,
         TipoUsuario tipo
 ) {
 
-    public DadosUsuarioCriado (Usuario dados){
+    public DadosUsuarioDetalhado(Usuario dados){
         this(dados.getId(), dados.getNome(), dados.getEmail(), dados.getTipo());
     }
 }
