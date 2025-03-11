e44fd2a8437d3ee62a096cabb0894911346581c1
Lucas Moreiram <lggm18@hotmail.com>
2025-03-11 19:09:16 -0300

    feat: adicionado endpoint para retornar usuário por id

--- src/main/java/com/fiap/ecr/api_marcacao_consultas/controller/UsuarioController.java
+++ src/main/java/com/fiap/ecr/api_marcacao_consultas/controller/UsuarioController.java
@@ -1,6 +1,6 @@
 package com.fiap.ecr.api_marcacao_consultas.controller;
 
-import com.fiap.ecr.api_marcacao_consultas.dto.DadosUsuarioCriado;
+import com.fiap.ecr.api_marcacao_consultas.dto.DadosUsuarioDetalhado;
 import com.fiap.ecr.api_marcacao_consultas.model.Usuario;
 import com.fiap.ecr.api_marcacao_consultas.service.UsuarioService;
 import com.fiap.ecr.api_marcacao_consultas.security.JwtTokenProvider;
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -23,8 +24,8 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public ResponseEntity<DadosUsuarioCriado> criarUsuario(@RequestBody Usuario usuario) {
-        return ResponseEntity.ok(new DadosUsuarioCriado(usuarioService.salvarUsuario(usuario)));
+    public ResponseEntity<DadosUsuarioDetalhado> criarUsuario(@RequestBody Usuario usuario) {
+        return ResponseEntity.ok(new DadosUsuarioDetalhado(usuarioService.salvarUsuario(usuario)));
     }
 
     @PostMapping("/login")
@@ -38,6 +39,15 @@ public class UsuarioController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosUsuarioDetalhado> retornarUsuario(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.procurarPorId(id);
+        return usuario.map(d -> ResponseEntity.ok(new DadosUsuarioDetalhado(d)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
 
 
 }
